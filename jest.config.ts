2097e9c24273ae193c0aefa7892c69eda8b1c7de
Elaard <bartek.dv.89@gmail.com>
2023-04-18 23:30:28 +0200

    test: set jest config

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
