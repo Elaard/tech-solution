e274ddef6b0edcfad109af7c4cc4f54d2b69de05
Elaard <bartek.dv.89@gmail.com>
2023-04-20 10:41:15 +0200

    refactor: change name of test utils function

--- src/testUtils/withProviders.tsx
+++ src/testUtils/withProviders.tsx
@@ -20,6 +20,6 @@ export function renderWithServicesProvider(children: JSX.Element) {
   return render(getWithServicesProvider(children));
 }
 
-export function renderWithServicesAndServicesFilterProviders(services: ServiceOffer, children: JSX.Element) {
+export function renderWithAllProviders(services: ServiceOffer, children: JSX.Element) {
   return renderWithServicesProvider(getWithServicesFilterProvider(services, children));
 }
